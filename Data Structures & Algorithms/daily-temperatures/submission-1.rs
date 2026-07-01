b3abc9df1991e91b1aaa26a42ad3c4c79d1de2ec
James Cenawood <jdewitte632@gmail.com>
2026-07-01 14:09:53 -0400

    Add: daily-temperatures - submission-1

---
+++ Data Structures & Algorithms/daily-temperatures/submission-1.rs
@@ -0,0 +1,16 @@
+impl Solution {
+    pub fn daily_temperatures(temperatures: Vec<i32>) -> Vec<i32> {
+        let n = temperatures.len();
+        let mut stack = Vec::new();
+        let mut more = vec![0; n];
+
+        for (i, &temp) in temperatures.iter().enumerate() {
+            while !stack.is_empty() && temp > temperatures[*stack.last().unwrap()] {
+                let j = stack.pop().unwrap();
+                more[j] = (i-j) as i32;
+            }
+            stack.push(i);
+        }
+        return more
+    }
+}
