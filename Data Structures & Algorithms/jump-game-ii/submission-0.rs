dade1148e7a3914d4f828562b0af01fa87e310fd
James Cenawood <jdewitte632@gmail.com>
2026-07-04 19:08:03 -0700

    Add: jump-game-ii - submission-0

---
+++ Data Structures & Algorithms/jump-game-ii/submission-0.rs
@@ -0,0 +1,16 @@
+impl Solution {
+    pub fn jump(nums: Vec<i32>) -> i32 {
+        let n : usize = nums.len();
+        let mut jumps : i32 = 0;
+        let mut current_jump_end : usize = 0;
+        let mut farthest : usize = 0;
+        for i in 0..n-1 {
+            farthest = max(farthest, i + (nums[i] as usize));
+            if i == current_jump_end {
+                jumps += 1;
+                current_jump_end = farthest;
+            }
+        }
+        jumps
+    }
+}
