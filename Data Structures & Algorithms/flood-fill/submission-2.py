2ee58829d154430fbe4e550726ae1368e56fd034
James Cenawood <jdewitte632@gmail.com>
2026-08-14 23:33:07 -0400

    Add: flood-fill - submission-2

---
+++ Data Structures & Algorithms/flood-fill/submission-2.py
@@ -0,0 +1,21 @@
+class Solution:
+    def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
+        n = len(image) - 1
+        m = len(image[0]) - 1
+
+        if image[sr][sc] == color:
+            return image
+
+        def dfs(i,j,c):
+            if i > n or j > m or i < 0 or j < 0:
+                return
+            elif image[i][j] != c:
+                return
+            else:
+                image[i][j] = color
+                neighbors = [(i+1,j), (i-1,j), (i,j+1), (i,j-1)]
+                for (n1,n2) in neighbors:
+                    dfs(n1,n2,c)
+        
+        dfs(sr, sc, image[sr][sc])
+        return image
