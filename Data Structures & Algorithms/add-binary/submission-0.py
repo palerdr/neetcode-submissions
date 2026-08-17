2a83b0f3c854813c71be6529af92a2962e82e086
James Cenawood <jdewitte632@gmail.com>
2026-08-17 16:44:28 -0400

    Add: add-binary - submission-0

---
+++ Data Structures & Algorithms/add-binary/submission-0.py
@@ -0,0 +1,18 @@
+class Solution:
+    def addBinary(self, a: str, b: str) -> str:
+        i,j = len(a)-1, len(b)-1
+        result = []
+
+        carry = 0
+        while i >= 0 or j >= 0 or carry > 0:
+            a_digit = int(a[i]) if i >= 0 else 0
+            b_digit = int(b[j]) if j >= 0 else 0
+            total = a_digit + b_digit + carry
+            result.append(total % 2)
+            carry = total // 2
+            i -= 1
+            j -= 1
+        
+        result.reverse()
+        return "".join(map(str, result))
+
